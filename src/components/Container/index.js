e08bde6d0c843293036a8e3e8bbf0878e5ae8964
Oskari Pöntinen <mail.morko@gmail.com>
2020-01-22 15:25:53 +0200

    added responsive margins to containers

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './index.scss'
 
 export default function Container({ children, className = '' }) {
   return <div className={`container ${className}`}>{children}</div>
