67d0662b2a382948bf1af4ec312fd818a1402103
Oskari Pöntinen <mail.morko@gmail.com>
2021-04-20 10:39:43 +0300

    fix: remove unused hooks

--- src/hooks/useGsap.js
+++
@@ -1,7 +0,0 @@
-import { gsap } from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-gsap.registerPlugin(ScrollTrigger)
-
-export default function useGsap() {
-  return { gsap, ScrollTrigger };
-}
