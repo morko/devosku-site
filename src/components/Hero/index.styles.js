f5841d323e22c8b2699b4cfd022fe1b1e1c19630
Oskari Pöntinen <mail.morko@gmail.com>
2021-04-26 09:57:10 +0300

    fix: hero text animation

--- src/components/Hero/index.styles.js
+++ src/components/Hero/index.styles.js
@@ -1,29 +1,55 @@
 const { createUseStyles } = require("react-jss");
 
 const useStyles = createUseStyles((theme) => ({
+  textbox: {
+    height: '100%',
+    alignSelf: 'flex-start',
+    zIndex: 1,
+    color: theme.lightColor,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
   title: {
     fontFamily: theme.primaryFont,
     fontSize: '3.5em',
     fontWeight: 400,
     lineHeight: '1.2',
     margin: 0,
+    opacity: 0,
+    animation: 'fadeInUp 2s',
+    animationFillMode: 'forwards',
   },
   subtitle: {
     fontFamily: theme.secondaryFont,
     fontWeight: 400,
     marginTop: '50px',
     fontSize: '1.125em',
-  },
-  textbox: {
-    height: '100%',
-    alignSelf: 'flex-start',
-    zIndex: 1,
+    opacity: 0,
     animation: 'fadeInUp 2s',
+    animationFillMode: 'forwards',
+    animationDelay: '0.5s',
+  },
+  callToAction: {
+    marginTop: '50px',
+    backgroundColor: theme.primaryColor,
     color: theme.lightColor,
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'flex-start',
+    borderRadius: '4px',
+    fontFamily: theme.primaryFont,
+    padding: '16px 14px 14px',
+    border: 'none',
+    fontWeight: 600,
+    letterSpacing: 1.5,
+    lineHeight: 1,
+    textDecoration: 'none',
+    opacity: 0,
+    animation: 'fadeInUp 2s',
+    animationFillMode: 'forwards',
+    animationDelay: '1s',
+    '&:hover': {
+      cursor: 'pointer',
+    }
   },
   illustration: {
     zIndex: 1000,
@@ -56,22 +82,6 @@ const useStyles = createUseStyles((theme) => ({
       alignItems: 'center',
     }
   },
-  callToAction: {
-    marginTop: '50px',
-    backgroundColor: theme.primaryColor,
-    color: theme.lightColor,
-    borderRadius: '4px',
-    fontFamily: theme.primaryFont,
-    padding: '16px 14px 14px',
-    border: 'none',
-    fontWeight: 600,
-    letterSpacing: 1.5,
-    lineHeight: 1,
-    textDecoration: 'none',
-    '&:hover': {
-      cursor: 'pointer',
-    }
-  }
 }))
 
 export default useStyles
